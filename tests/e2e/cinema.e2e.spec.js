4b8e3c55099f452fdbfaaa38587a0795927f523d
Nipruk Viktoria <niprukviktoria@gmail.com>
2026-04-08 21:49:50 +0300

    fix: rename main.html to index.html

--- tests/e2e/cinema.e2e.spec.js
+++ tests/e2e/cinema.e2e.spec.js
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('успішне бронювання місць', async ({ page }) => {
-  await page.goto('http://127.0.0.1:5500/main.html');
+  await page.goto('http://127.0.0.1:5500/index.html');
 
   const seats = page.locator('.seat:not(.occupied)');
   await seats.nth(0).click();
